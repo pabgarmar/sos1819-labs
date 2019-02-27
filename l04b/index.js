b922450cda11288071c950ba7fcc80907d5adc64
sos1819pgm <pabgarmar95@hotmail.com>
2019-02-27 13:39:32 +0000

    fix

--- l04b/index.js
+++ l04b/index.js
@@ -1,4 +1,16 @@
 var cool = require("cool-ascii-faces");
 
+var express = require("express");
 
-console.log(cool());
+var app= express();
+
+var port = process.env.PORT || 8080;
+
+app.get("/", (request, response) => {
+    response.send(cool());
+    console.log("New request received");
+});
+
+app.listen(port, () => {
+    console.log("Server ready!")
+});
