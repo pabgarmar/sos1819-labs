77e71f8fa659f1307a07dadaabc16c493cb79035
sos1819pgm <pabgarmar95@hotmail.com>
2019-03-21 11:13:50 +0000

    jscript

--- l07/index.js
+++ l07/index.js
@@ -1,14 +1,125 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 
 var app = express();
-const port = process.env.PORT || 8080;
 
+app.use(bodyParser.json());
 
-app.get("/", (req,res) =>{
-    console.log("New request!");
-    res.send("<html><body>Hi there!</body></html>");
+var port = process.env.PORT || 8080;
+
+var contacts = [{
+    name: "peter",
+    phone: "123456",
+    email: "peter@peter.com"
+}, {
+    name: "paul",
+    phone: "3333",
+    email: "paul@paul.com"
+}];
+
+// GET /contacts/
+
+app.get("/contacts", (req,res)=>{
+    res.send(contacts);
 });
 
+
+// POST /contacts/
+
+app.post("/contacts", (req,res)=>{
+    
+    var newContact = req.body;
+    
+    contacts.push(newContact)
+    
+    res.sendStatus(201);
+});
+
+
+// DELETE /contacts/
+
+app.delete("/contacts", (req,res)=>{
+    
+    contacts =  [];
+
+    res.sendStatus(200);
+});
+
+
+// GET /contacts/peter
+
+app.get("/contacts/:name", (req,res)=>{
+
+    var name = req.params.name;
+
+    var filteredContacts = contacts.filter((c) =>{
+       return c.name == name; 
+    })
+    
+    if (filteredContacts.length >= 1){
+        res.send(filteredContacts[0]);
+    }else{
+        res.sendStatus(404);
+    }
+
+});
+
+
+// PUT /contacts/peter
+
+app.put("/contacts/:name", (req,res)=>{
+
+    var name = req.params.name;
+    var updatedContact = req.body;
+    var found = false;
+
+    var updatedContacts = contacts.map((c) =>{
+    
+        if(c.name == name){
+            found = true;
+            return updatedContact;
+        }else{
+            return c;            
+        }
+ 
+    });
+    
+    if (found == false){
+        res.sendStatus(404);
+    }else{
+        contacts = updatedContacts;
+        res.sendStatus(200);
+    }
+
+});
+
+
+// DELETE /contacts/peter
+
+app.delete("/contacts/:name", (req,res)=>{
+
+    var name = req.params.name;
+    var found = false;
+
+    var updatedContacts = contacts.filter((c) =>{
+        
+            if(c.name == name)  
+                found = true;
+        
+            return c.name != name;
+    });
+    
+    if (found == false){
+        res.sendStatus(404);
+    }else{
+        contacts = updatedContacts;
+        res.sendStatus(200);
+    }
+
+});
+
+
+
 app.listen(port, () => {
-    console.log("Ready in port "+port);
+    console.log("Super server ready on port " + port);
 });
